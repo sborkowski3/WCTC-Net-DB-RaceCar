219cc36471aa42a4e59a5ec8d778425b00f28530
Stephanie Borkowski <sborkowski3@my.wctc.edu>
2020-03-09 20:56:36 -0500

    Created a new OutLander class

---
+++ RaceTrack/Cars/OutLander.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+
+    public class OutLander : RaceCar
+    {
+        public OutLander()
+        {
+            Name = "OutLander";
+            TopSpeed = 90;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} roars to life!!");
+        }
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} squeals to a stop!");
+            base.Brake();
+        }
+    }
+}
